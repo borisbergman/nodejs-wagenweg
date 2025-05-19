0704bb2c85141f1ea5d67f912233db9be5f48d94
boris <b.c.bergman@uu.nl>
2025-05-19 18:23:01 +0200

    wrong proto-dir

--- src/app.js
+++ src/app.js
@@ -58,7 +58,7 @@ app.use(express.json());
 
 app.route('/wagenweg170B/solenoid/')
   .post(function (req, res, next) {  
-      protobuf.load("../proto/solenoid.proto", function(err, root) {
+      protobuf.load("proto/solenoid.proto", function(err, root) {
         var solenoidMessage = root.lookupType("Solenoid");
         var payload = {id : req.body.solenoidid, time : req.body.time};
         var message = solenoidMessage.create(payload);
